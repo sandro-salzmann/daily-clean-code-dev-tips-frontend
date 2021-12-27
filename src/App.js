90ff1415ea2b788964b477950c91d700590dec9a
Sandro Salzmann <sandro.salzmann@stud.hslu.ch>
2021-12-27 22:35:07 +0100

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { SettingsPage } from "./pages/SettingsPage";
+import { SetupPage } from "./pages/SetupPage";
+import { TipsPage } from "./pages/TipsPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<HomePage />} />
+        <Route path="setup" element={<SetupPage />} />
+        <Route path="settings" element={<SettingsPage />} />
+        <Route path="tips" element={<TipsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SetupPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SetupPage = () => {
+  return (
+    <div>
+      <h1>Setup</h1>
+    </div>
+  );
+};
