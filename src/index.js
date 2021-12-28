70a282e89be0e1233fb5a671ef21643e409a4574
Sandro Salzmann <sandro.salzmann@stud.hslu.ch>
2021-12-28 02:04:48 +0100

    Fix hot replacements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+if (module.hot) {
+  module.hot.accept();
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
