67898e90b71c92a06bb9791ecba46a1de9ed335c
Sandro Salzmann <sandro.salzmann@stud.hslu.ch>
2022-02-09 21:28:17 +0100

    Show navigation and header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { useContext } from "react";
+import { useMediaQuery } from "react-responsive";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import { Header } from "./components/Header";
+import { MobileNavigation } from "./components/MobileNavigation";
+import { ConfigContext } from "./ConfigContextProvider";
 import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 import { SettingsPage } from "./pages/SettingsPage";
 import { SetupPage } from "./pages/SetupPage";
 import { TipsPage } from "./pages/TipsPage";
-import { NotFoundPage } from "./pages/NotFoundPage";
-import { Container } from "semantic-ui-react";
 
 function App() {
+  const isMobile = useMediaQuery({ query: "(max-width: 560px)" });
+  const { isConfigured } = useContext(ConfigContext);
+
   return (
     <div style={{ padding: "24.571px 0px" }}>
       <BrowserRouter>
         <Container text>
+          <Header />
           <Routes>
             <Route path="" element={<HomePage />} />
             <Route path="setup" element={<SetupPage />} />
@@ -19,6 +28,7 @@ function App() {
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Container>
+        {isMobile && isConfigured && <MobileNavigation />}
       </BrowserRouter>
     </div>
   );
